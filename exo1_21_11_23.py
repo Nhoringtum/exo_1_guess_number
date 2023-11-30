915fe9e09248766bbde4cd9845e433dd3d3a0c2e
Nodeions <147034141+Nhoringtum@users.noreply.github.com>
2023-11-30 18:52:09 +0100

    Update exo1_30_11_23.py
    Optimisation

--- exo1_21_11_23.py
+++ exo1_21_11_23.py
@@ -1,16 +1,14 @@
 import random
 import tools
 
-guess: int = 1
-isPlayingGame: bool = True
 maxGuess: int = 5
 
 
-def random_Number(x: int, y: int) -> int:
+def random_number(x: int, y: int) -> int:
     i: int = random.randint(x, y)
     return i
 
-
+# Fonction qui renvoit True si ce que le joueur à écrit correspond au nombre aléatoire à deviner
 def compare_int(x: int, y: int) -> bool:
     while True:
         if x > y:
@@ -23,33 +21,41 @@ def compare_int(x: int, y: int) -> bool:
             print("Bravo vous avez trouvé !\n")
             return True
 
-
-while isPlayingGame:
-    borneA: int = tools.ask_int()
-    borneB: int = tools.ask_int()
-
-    while borneA == borneB:
-        print("Bornes trop proches, veuillez réessayer\n")
+def LaunchGame():
+    isPlayingGame: bool = True
+    while isPlayingGame:
+        guess: int = 0
+        # fonction ask_int vérifie si ce que le joueur entre est bien un int
         borneA: int = tools.ask_int()
         borneB: int = tools.ask_int()
 
-    borneA, borneB = tools.reverse_number(borneA, borneB)
-    randomNumber: int = random_Number(borneA, borneB)
-    guessNumber: int = tools.ask_int_in_range(borneA, borneB)
-    isNumBerFind: bool = compare_int(guessNumber, randomNumber)
+        # Boucle sécurité pour éviter que le joueur mettent le même numéro, a terme on pourra mettre un écart minimum
+        while borneA == borneB:
+            print("Bornes trop proches, veuillez réessayer\n")
+            borneA: int = tools.ask_int()
+            borneB: int = tools.ask_int()
 
-    while not isNumBerFind:
-        if guess < maxGuess:
-            print(f"Vous n'avez plus que {maxGuess - guess} essais :\n")
-            guessNumber: int = ask_int_in_range(borneA, borneB)
-            isNumBerFind: bool = compare_int(guessNumber, randomNumber)
+        # Fonction reverse_number met dans l'ordre du plus petit au plus grand deux int du sens inverse
+        borneA, borneB = tools.reverse_number(borneA, borneB)
+        randomNumber: int = random_number(borneA, borneB)
+        guessNumber: int = tools.ask_int_in_range(borneA, borneB)
+        isNumBerFind: bool = compare_int(guessNumber, randomNumber)
+
+        # Boucle pour continuer à deviner le nombre
+        while not isNumBerFind:
             guess += 1
-        else:
-            print(f"Perdu ! Le nombre a trouver était : {randomNumber}")
-            break
+            if guess < maxGuess:
+                print(f"Vous n'avez plus que {maxGuess - guess} essais :\n")
+                guessNumber: int = tools.ask_int_in_range(borneA, borneB)
+                isNumBerFind: bool = compare_int(guessNumber, randomNumber)
+            else:
+                print(f"Perdu ! Le nombre a trouver était : {randomNumber}")
+                break
 
-    again: str = input('Voulez-vous rejouer ?\nType "y" or "n" \n')
-    if again == "y":
-        guess = 0
-    else:
-        isPlayingGame = False
+        again: str = input('Voulez-vous rejouer ?\nType "y" or "n" \n')
+        if again == "y":
+            guess = 0
+        else:
+            isPlayingGame = False
+
+LaunchGame()
